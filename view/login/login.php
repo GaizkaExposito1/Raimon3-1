4ed09a17919e0b2c5d8079c819dd94b62c528998
Maura <56032847+maura23@users.noreply.github.com>
2021-10-18 08:24:36 +0200

    crear
    Login/Register

---
+++ view/login/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/loginStyle.css.css"> 
+<script src="js/login.js"></script>
+</head>
+<body>
+	
+
+<div class="page">
+	<div class="container">
+	  <div class="left">
+		<div class="login">Iniciar Sesión</div>
+		<div class="eula">Bienvenido al arbol de los deseos!!</div>
+	  </div>
+	  <div class="right"> 
+		<div class="form">
+		  <label for="suario"> Usuario</label>
+		  <input type="text" id="text">
+		  <label for="password">Contraseña</label>
+		  <input type="password" id="password">
+		  <input type="submit" id="submit" value="Aceptar">
+		</div>
+	  </div>
+	</div>
+  </div>
+</body>
+</html>
---
+++ view/registro/registro.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/registroStyle.css">
+<script src="js/login.js"></script>
+</head>
+<body>
+	
+
+<div class="page">
+	<div class="container">
+	  <div class="left">
+		<div class="register">Registro!!</div>
+		<div class="eula">Bienvenido al arbol de los deseos!!</div>
+	  </div>
+	  <div class="right"> 
+		<div class="form">
+          <label for="dni">Dni</label>
+		  <input type="text" id="text">
+
+		  <label for="password">Contraseña</label>
+		  <input type="password" id="password">
+
+		  <label for="password">Confirmar Contraseña </label>
+		  <input type="password" id="password">
+
+		  <label for="email"> Correo Electrónico</label>
+		  <input type="email" id="email">
+
+
+		  <label for="userName">Usuario</label>
+		  <input type="text" id="text">
+		 
+
+		  <input type="submit" id="submit" value="Aceptar">
+		</div>
+	  </div>
+	</div>
+  </div>
+</body>
+</html>
