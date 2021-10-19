2d764c3075ae6e9b685c66cafd7954d37af55fda
Maura <56032847+maura23@users.noreply.github.com>
2021-10-19 13:50:53 +0200

    sadhdas
    bootstrap carousel

---
+++ view/principal/principal.js
@@ -0,0 +1,12 @@
+
+$('#myCarouselCustom').carousel();
+
+
+$("#prevBtn").click(function(){
+    $("#myCarouselCustom").carousel("anterior");
+});
+
+$("#nextBtn").click(function(){
+    $("#myCarouselCustom").carousel("siguiente");
+});
+
--- view/principal/principal.php
+++ view/principal/principal.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Principal/style.css">
     <script src="../principal.js"></script>
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+    <!-- Compiled and minified Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<!-- Minified JS library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
 </head>
 <body>
 
@@ -41,39 +45,60 @@
      </div>
 
  <!--Boststrap carousel-->
-
-
- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div id="myCarouselCustom" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
     <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
-  </div>
+        <li data-target="#myCarouselCustom" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarouselCustom" data-slide-to="1"></li>
+        <li data-target="#myCarouselCustom" data-slide-to="2"></li>
+    </ol>
 
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+           <p class="lorem">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Rem dolorum libero ea aliquid, odio, ipsam beatae laudantium
+               illum temporibus culpa quibusdam quasi atque sunt incidunt alias
+                omnis suscipit. Nam, tenetur.</p>
+             <p class="lorem">like 40</p>
+         <div class="carousel-caption">
+            
+            </div>
+         </div>
+  
+         <div class="item">
+          <p class="lorem">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Rem dolorum libero ea aliquid, odio, ipsam beatae laudantium
+               illum temporibus culpa quibusdam quasi atque sunt incidunt alias
+                omnis suscipit. Nam, tenetur.</p>
+                <p class="lorem">like 100</p>
+             <div class="carousel-caption">
+         
+            </div>
+        </div>
+        
+        <div class="item">
+         <p class="lorem">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Rem dolorum libero ea aliquid, odio, ipsam beatae laudantium
+               illum temporibus culpa quibusdam quasi atque sunt incidunt alias
+                omnis suscipit. Nam, tenetur.</p>
+                <p class="lorem">like 500</p>
+               <div class="carousel-caption">
+           
+            </div>
+        </div>
+    </div>
 
+    <!-- Controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="antrior">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="siguiente">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
-
-
-
 
 
 
@@ -81,9 +106,3 @@
 
 </body>
 </html>
-
-
-<script>
-
-
-</script>
