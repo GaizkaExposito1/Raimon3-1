b7e2ae6510d10c37041272253b8fce4a1ed51841
Santsikai <76107411+Santsikai@users.noreply.github.com>
2021-10-20 10:30:44 +0200

    dd

--- view/login/login.php
+++ view/login/login.php
@@ -2,14 +2,16 @@
 <?php
 
 if (isset($lang)) {
-    $url_prefix="./view";
-	$url_prefixU="./view";
-	$url_prefixF=".";
+    $url_prefix="../view/login/";
+	$url_prefixU="../view";
+	$url_prefixF="..";
 	$url_prefixC=".";
+	$url_prefixBD="./";
 }else{
     $lang = "es";
-    $url_prefix=".";
-	$url_prefixF="../..";
+    $url_prefix="./";
+	$url_prefixBD="../../";
+	$url_prefixF=".";
 	$url_prefixU="..";
 	$url_prefixC="..";
 }
@@ -23,8 +25,8 @@ require_once ("$url_prefixU/Language/lang_".$lang.".php");
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet"  href="<?php echo "$url_prefix"?>/loginStyle.css"> 
-	<script src="<?php echo "$url_prefix"?>/login.js"></script>
+	<link rel="stylesheet"  href="<?php echo "$url_prefix"?>loginStyle.css"> 
+	<!--<script src="<?php echo "$url_prefix"?>/login.js"></script>-->
 </head>
 <body>
 	
@@ -37,7 +39,7 @@ require_once ("$url_prefixU/Language/lang_".$lang.".php");
 		<input type="button" id="mostrar" name="mostrar" value="Mostrar" onclick=mostrarCaja() ></input>
 	  </div>
 	  <div class="right" id="right"> 
-		<form class="form" action="<?php echo "$url_prefixF"?>/controller/actions/inicioSesion.php" method="post">
+		<form class="form" action="<?php echo "$url_prefixF"?>/controller/login.php" method="post">
 		  <label for="suario"><?php echo $language["USER"]; ?></label>
 		  <input type="text" id="text">
 		  <label for="password"><?php echo $language["PASS"]; ?></label>
