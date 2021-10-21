6d5c0b29fcabc402819c31ee79b0617c83566b4c
GaizkaExposito1 <76107546+GaizkaExposito1@users.noreply.github.com>
2021-10-21 12:37:20 +0200

    sdag

--- view/perfil/perfil.js
+++ view/perfil/perfil.js
@@ -1,13 +1,22 @@
+/*
 function mostrarCaja() {
     let MostrarForm = document.getElementById("right");
     let cambiarTamañoLeft = document.getElementById("left");
     let ocultarBoton = document.getElementById("mostrar");
     MostrarForm.style.visibility="visible";
     cambiarTamañoLeft.style.height="88%";
-    ocultarBoton.style.visibility="";
-}
+    ocultarBoton.style.visibility="hidden";
+}*/
 
 $(document).ready(function(){
+    $("[value=mostrar]").click(function(){
+        let MostrarForm=$("#right");
+        let cambiarTamañoLeft=$("#left");
+        let ocultarBoton=$("#mostrar");
+        MostrarForm.css("visibility", "visible");
+        cambiarTamañoLeft.css("height", "88%");
+        ocultarBoton.css("visibility", "hidden");
+    });
     $("#cambiarContraseña").click(function(){
         $("#editarContraseña").css("display", "inline");
         $("#editarEmail").css("display", "none");
@@ -15,7 +24,6 @@ $(document).ready(function(){
         $('input[type="text"]').val('');
         $('input[type="email"]').val('');
     });
-
     $("#cambiarEmail").click(function(){
         $("#editarContraseña").css("display", "none");
         $("#editarEmail").css("display", "inline");
@@ -23,7 +31,6 @@ $(document).ready(function(){
         $('input[type="password"]').val('');
         $('input[type="text"]').val('');
     });
-
     $("#cambiarUsername").click(function(){
         $("#editarContraseña").css("display", "none");
         $("#editarEmail").css("display", "none");
@@ -31,6 +38,4 @@ $(document).ready(function(){
         $('input[type="password"]').val('');
         $('input[type="email"]').val('');
     });
-
-
 });
--- view/perfil/perfil.php
+++ view/perfil/perfil.php
@@ -3,109 +3,101 @@ require_once "../../model/accesoBD.class.php";
 require_once "../../model/clases/mensaje.class.php";
 
 $bd= new AccesoBd();
- ?>
+?>
 
 
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="perfil.css">
-	<script src="jquery.min.js"></script>
-	<script src="perfil.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="perfil.css">
+<script src="jquery.min.js"></script>
+<script src="perfil.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
 
 </head>
 
-	
+
 <article>
-<div class="page">
-	<div class="container">
-	  <div class="left" id="left">
-		<div class="perfil">Perfil</div>
-		<div class="eula">Aqui podras editar tu perfil</div>
-		<input type="button" id="mostrar" value="mostrar" onclick="mostrarCaja()">
-
-
-
-	  </div>
-	  <div class="right" id="right"> 
-		  <div id="imgPerfil">
-	  		<img src="../assets/cat.jpg" alt="imagen de perfil">
-		  </div>
-		  <div id="datosPerfil">
-			<p>Usuario : NOMBREUSUARIO</p>
-			<p>Email: EMAILUSUARIO</p>
-			<p>Contraseña:  </p>
-			<button id="cambiarContraseña">Cambiar Contraseña</button>
-			<button id="cambiarUsername">Cambiar Usuario</button>
-			<button id="cambiarEmail">Cambiar Email</button>
-			<br/>
-			<a href="#mensajesAprobados" id="mensajesAprobados">Mensajes Aprobados</a>
-			<a href="#mensajesNoAprobados"  id="mensajesDenegados">Mensajes Denegados</a>
-			<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vercontraseñaModal">
-  Ver Contraseña
-</button>
-
-<!-- Modal Ver Contraseña -->
-<div class="modal fade" id="vercontraseñaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Ver Contraseña</h5>
-      </div>
-      <div class="modal-body">
-        La contraseña de tu cuenta es: 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-		  </div>
-			  <div id="editarDatsos">
-			  <form action="" id="editarContraseña" method="post">
-			  <label for="Contraseña">Contraseña Antiguo</label>
-		 	  <input type="password" id="email">
-			  <label for="Contraseña">Nuevo Contraseña</label>
-			  <input type="password" id="NewEmail">
-			   <label for="Contraseña">Repite Nuevo Contraseña</label>
-			   <input type="password" id="repeatNewEmail">
-			   <p><input type="submit" id="submit" value="Enviar"></p>
-			</form>
-			<form action="" id="editarEmail" method="post">
-			  <label for="email">Email Antiguo</label>
-		 	  <input type="email" id="email">
-			  <label for="email">Nuevo Email</label>
-			  <input type="email" id="email">
-			   <p><input type="submit" id="submit" value="Enviar"></p>
-			</form>
-			<form action="" id="editarUsername" method="post">
-			  <label for="Username">Username Antiguo</label>
-		 	  <input type="text" id="username">
-			  <label for="Username">Nuevo Username</label>
-			  <input type="text" id="username">
-			   <p><input type="submit" id="submit" value="Enviar"></p>
-			</form>
-		  </div>
-
-	  </div>
+	<div class="page">
+		<div class="container">
+			<div class="left" id="left">
+				<div class="perfil">Perfil</div>
+				<div class="eula">Aqui podras editar tu perfil</div>
+				<input type="button" id="mostrar" value="mostrar" onclick="mostrarCaja()">
+			</div>
+			<div class="right" id="right"> 
+				<div id="imgPerfil">
+					<img src="../assets/cat.jpg" alt="imagen de perfil">
+				</div>
+				<div id="datosPerfil">
+					<p>Usuario : NOMBREUSUARIO</p>
+					<p>Email: EMAILUSUARIO</p>
+					<p>Contraseña:  </p>
+					<button id="cambiarContraseña">Cambiar Contraseña</button>
+					<button id="cambiarUsername">Cambiar Usuario</button>
+					<button id="cambiarEmail">Cambiar Email</button>
+					<br/>
+					<a href="#mensajesAprobados" id="mensajesAprobados">Mensajes Aprobados</a>
+					<a href="#mensajesNoAprobados"  id="mensajesDenegados">Mensajes Denegados</a>
+					<!-- Button trigger modal -->
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#vercontraseñaModal">
+					Ver Contraseña
+					</button>
+					<!-- Modal Ver Contraseña -->
+					<div class="modal fade" id="vercontraseñaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">Ver Contraseña</h5>
+								</div>
+								<div class="modal-body">
+									La contraseña de tu cuenta es: 
+								</div>
+								<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="editarDatsos">
+					<form action="" id="editarContraseña" method="post">
+						<label for="Contraseña">Contraseña Antiguo</label>
+						<input type="password" id="email">
+						<label for="Contraseña">Nuevo Contraseña</label>
+						<input type="password" id="NewEmail">
+						<label for="Contraseña">Repite Nuevo Contraseña</label>
+						<input type="password" id="repeatNewEmail">
+						<p><input type="submit" id="submit" value="Enviar"></p>
+					</form>
+					<form action="" id="editarEmail" method="post">
+						<label for="email">Email Antiguo</label>
+						<input type="email" id="email">
+						<label for="email">Nuevo Email</label>
+						<input type="email" id="email">
+						<p><input type="submit" id="submit" value="Enviar"></p>
+					</form>
+					<form action="" id="editarUsername" method="post">
+						<label for="Username">Username Antiguo</label>
+						<input type="text" id="username">
+						<label for="Username">Nuevo Username</label>
+						<input type="text" id="username">
+						<p><input type="submit" id="submit" value="Enviar"></p>
+					</form>
+				</div>
+			</div>
+		</div>
 	</div>
-  </div>
 </article>
-
-
-
 <!--
-	datos perfil tic
+datos perfil tic
 camios de perfil (foto, contraseña, email, username) tic
-enlace para ver mensajes aprobados 
-enlace para ver mensajes no aprobados
+enlace para ver mensajes aprobados tic
+enlace para ver mensajes no aprobados tic
 ------------------------
 rol=2
 crear Admins
