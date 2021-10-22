d55b2fb6ad0235b70e1683f39a27ef284e4ad6d6
GaizkaExposito1 <76107546+GaizkaExposito1@users.noreply.github.com>
2021-10-22 12:19:57 +0200

    registro y login arreglados

---
+++ view/registro/registro.js
@@ -0,0 +1,22 @@
+/*$(document).ready(function(){
+    $("[value=mostrar]").click(function(){
+        let MostrarForm=$("#right");
+        let cambiarTamañoLeft=$("#left");
+        let ocultarBoton=$("#mostrar");
+        MostrarForm.css("visibility", "visible");
+        cambiarTamañoLeft.css("height", "88%");
+        ocultarBoton.css("visibility", "hidden");
+    });
+  
+  });*/
+    
+
+
+function mostrarCaja() {
+    let MostrarForm = document.getElementById("right");
+    let cambiarTamañoLeft = document.getElementById("left");
+    let ocultarBoton = document.getElementById("mostrar");
+    MostrarForm.style.visibility="visible";
+    cambiarTamañoLeft.style.height="88%";
+    ocultarBoton.style.visibility="";
+}
