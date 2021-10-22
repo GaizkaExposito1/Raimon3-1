c78810993fa3e83be212df8a56e02e7eb4253590
Santsikai <76107411+Santsikai@users.noreply.github.com>
2021-10-22 13:33:22 +0200

    j

--- view/principal/principal.php
+++ view/principal/principal.php
@@ -80,9 +80,9 @@
     </a>
     <!-------Links para ir a otras paginas--------->
     <div id="links">
-        <a href="/Raimon3-1/controller/conexion/crearMensaje.php"><button>Crear Mensaje</button></a>
-        <a href="/Raimon3-1/controller/conexion/mensajesAprobados.php"><button>Mensajes Aprobados</button></a>
-        <a href="/Raimon3-1/controller/conexion/mensajesNoAprobados.php"><button>Mensajes Aprobados</button></a>
+        <a href="/Raimon3-1/index.php?section=crearMensaje"><button>Crear Mensaje</button></a>
+        <a href="/Raimon3-1/index.php?section=mensajesAprobados"><button>Mensajes Aprobados</button></a>
+        <a href="/Raimon3-1/index.php?section=mensajesNoAprobados"><button>Mensajes No Aprobados</button></a>
     </div>
 </div>
 
