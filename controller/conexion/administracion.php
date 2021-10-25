ace382197363d7ac5db1384f5149fe10ff9a4a45
Santsikai <76107411+Santsikai@users.noreply.github.com>
2021-10-25 17:52:36 +0200

    s

--- controller/conexion/administracion.php
+++ controller/conexion/administracion.php
@@ -7,6 +7,7 @@ $bd= new AccesoBd();
 
 //capado
 if(isset($_SESSION['usuario'])){
+    if($_SESSION['usuario']->rol==2){
 //redireccion
-include "./view/administracion/administracion.php";}
+include "./view/administracion/administracion.php";}}
 ?>
