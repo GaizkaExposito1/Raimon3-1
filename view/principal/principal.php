3fee6eec018818f713cbe3b7154e34bfa7032831
Maura <56032847+maura23@users.noreply.github.com>
2021-10-27 12:52:42 +0200

    sas

--- view/principal/principal.php
+++ view/principal/principal.php
@@ -10,11 +10,54 @@
 					}
 					?>
     </select>
-   
+
+
+
+    <div class="arbolBotones"> 
+    
+    <img id="imgArbol" src="./view/assets/Arbol.png" alt="" >
+
+    <img id="hoja1" src="./view/assets/hoja1.png" alt="" srcset="">
+    <img id="hoja2" src="./view/assets/hoja2.png" alt="" srcset="">
+    <img id="hoja3" src="./view/assets/hoja3.png" alt="" srcset="">
+    <img id="hoja4" src="./view/assets/hoja4.png" alt="" srcset="">
+    <img id="hoja5" src="./view/assets/hoja5.png" alt="" srcset="">
+    <img id="hoja6" src="./view/assets/hoja6.png" alt="" srcset="">
+    <img id="hoja7" src="./view/assets/hoja7.png"  alt="" srcset="">
+    <img id="hoja8" src="./view/assets/hoja8.png"  alt="" srcset="">
+    <img id="hoja9" src="./view/assets/hoja9.png"  alt="" srcset="">
+    <img id="hoja10" src="./view/assets/hoja10.png"  alt="" srcset="">
+    <img id="hoja11" src="./view/assets/hoja11.png"  alt="" srcset="">
+    <img id="hoja12"src="./view/assets/hoja1.png"  alt="" srcset="">
+    <img id="hoja13" src="./view/assets/hoja2.png"  alt="" srcset="">
+    <img id="hoja14" src="./view/assets/hoja3.png"  alt="" srcset="">
+    <img id="hoja15" src="./view/assets/hoja4.png"  srcset="">
+    <img id="hoja16" src="./view/assets/hoja5.png"  alt="" srcset="">
+    <img id="hoja17" src="./view/assets/hoja6.png"  alt="" srcset="">
+    <img id="hoja18" src="./view/assets/hoja7.png"  alt="" srcset="">
+    <img id="hoja19" src="./view/assets/hoja8.png" srcset="">
+    <img id="hoja20" src="./view/assets/hoja9.png"  alt="" srcset="">
+    <img id="hoja21" src="./view/assets/hoja11.png"  alt="" srcset="">
+    <img id="hoja22" src="./view/assets/hoja1.png"  alt="" srcset="">
+    <img id="hoja23" src="./view/assets/hoja6.png"  alt="" srcset="">
+    <img id="hoja24" src="./view/assets/hoja7.png"  alt="" srcset="">
+    <img id="hoja25" src="./view/assets/hoja8.png" srcset="">
+    <img id="hoja26" src="./view/assets/hoja9.png"  alt="" srcset="">
+    <img id="hoja27" src="./view/assets/hoja11.png"  alt="" srcset="">
+    <img id="hoja28" src="./view/assets/hoja1.png"  alt="" srcset="">
+    <img id="hoja29" src="./view/assets/hoja5.png"  alt="" srcset="">
+    <img id="hoja30" src="./view/assets/hoja6.png"  alt="" srcset="">
+    <img id="hoja31" src="./view/assets/hoja7.png"  alt="" srcset="">
+  
+    
+
+</div>
+   <!--
     <div class="arbolBotones"> 
     
         <img id="imgArbol" src="./view/assets/Arbol.png" alt="" >
     </div>
+                -->
                 
 
   
