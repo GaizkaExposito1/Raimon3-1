553dfadee6b1ba55869b23731a703409163fd5e5
GaizkaExposito1 <76107546+GaizkaExposito1@users.noreply.github.com>
2021-10-27 09:51:49 +0200

    12345

--- view/verMensaje/verMensaje.php
+++ view/verMensaje/verMensaje.php
@@ -17,9 +17,11 @@ if(isset($_SESSION['error'])){
                 <h4 id="creadorMensaje">Mensaje escrito por CreadorMensaje</h4>
 				<p id="mensaje">Mensaje:</p>
 				<p id="estado">Estado de mensaje:</p>
-                <p>
+				
+                <p id="like">
                 <a href="#" title="Love it" class="btn btn-counter" data-count="0"><span>&#x2764;</span></a> 
                 </p>
+				<a class='delete' href='#wii'>Eliminar Mensaje</a>
 			</div>
 		</div>
 	</div>
