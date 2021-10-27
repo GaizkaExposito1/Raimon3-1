34fe25c32933d0d6c17f59c1bf81f97fc6a2dd02
GaizkaExposito1 <76107546+GaizkaExposito1@users.noreply.github.com>
2021-10-27 09:12:59 +0200

    n

--- view/verMensaje/verMensaje.php
+++ view/verMensaje/verMensaje.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <section>
     <a href="$_SERVER['HTTP_REFERER']">Volver</a>
     <div>
@@ -14,3 +15,29 @@
 <p>Número de Likes: </p></p><?php $sms->numLikes ?>
 </div>
 </section>
+=======
+<?php
+if(isset($_SESSION['error'])){
+		echo "<div id='alert' class='alert alert-danger' role='alert'>".$_SESSION['error']."</div>";
+		unset($_SESSION['error']);
+}
+?>
+<article>
+	<div class="page">
+		<div class="container">
+			<div class="left" id="left">
+				<div class="perfil"><?php echo $language["PERFIL"]; ?></div>
+				<div class="eula"><?php echo $language["TXT_PERFIL"]; ?></div>
+				<input type="button" id="mostrar" value="mostrar" onclick="mostrarCaja()">
+			</div>
+			<div class="right" id="right">
+                <p>Mensaje escrito por CreadorMensaje</p>
+				<p>Mensaje:</p>
+                <p>
+                <a href="#" title="Love it" class="btn btn-counter" data-count="0"><span>&#x2764;</span></a> 
+                </p>
+			</div>
+		</div>
+	</div>
+</article>
+>>>>>>> Stashed changes
